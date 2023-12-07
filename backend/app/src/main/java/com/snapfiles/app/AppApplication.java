d671236bbf0db0b347c9c0ce70efb9a007b28956
sinchana2000 <46958639+sinchana2000@users.noreply.github.com>
2023-12-07 18:03:57 +0000

    config for test db

--- backend/app/src/main/java/com/snapfiles/app/AppApplication.java
+++ backend/app/src/main/java/com/snapfiles/app/AppApplication.java
@@ -1,16 +1,29 @@
 package com.snapfiles.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @ComponentScan("com.snapfiles.app.*")
-public class AppApplication {
+public class AppApplication implements CommandLineRunner{
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-
+	@Override
+    public void run(String... args) throws Exception {
+        String sql = "SELECT * from test";
+         
+        int rows = jdbcTemplate.update(sql);
+        if (rows > 0) {
+            System.out.println("A new row has been inserted--"+rows);
+        }
+    }
 }
